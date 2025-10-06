1f9eac59c3096159c451d084d6f2b7874a1aa38d
Pedro henrique <pedrofeliz2701@gmail.com>
2025-10-06 11:51:46 -0300

    UPDATE: Criando programa JS e alteração fonte REM CSS

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-  const inputTexto = document.getElementById("input-text");
+/*   const inputTexto = document.getElementById("input-text");
   const botaoMaiuscula = document.getElementById("Maiuscula");
   const botaoMinuscula = document.getElementById("minuscula");
   const resultado = document.getElementById("result");
@@ -12,3 +12,16 @@
     const texto = inputTexto.value;
     resultado.textContent = texto.toLowerCase();
   });
+ */
+
+window.document.addEventListener("DOMContentLoaded", function(){
+  window.document.querySelector("#btnMaiuscula").addEventListener("click", function(){
+    let inputText = document.querySelector("#input-text").value
+    document.querySelector("#result").innerHTML = inputText.toUpperCase()
+  })
+
+    window.document.querySelector("#btnminuscula").addEventListener("click", function(){
+    let inputText = document.querySelector("#input-text").value
+    document.querySelector("#result").innerHTML = inputText.toLowerCase()
+  })
+})
